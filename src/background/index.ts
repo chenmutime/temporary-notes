6508651a000846027712c26955b587b18010bb5d
chenmutime <chenmutime@outlook.com>
2023-08-19 17:48:05 +0800

    ww

--- src/background/index.ts
+++ src/background/index.ts
@@ -12,7 +12,7 @@ chrome.contextMenus.onClicked.addListener(function (info) {
     const url = info?.pageUrl;
     chrome.windows.create({
         type: 'popup',
-        url: '../src/options/index.html?selectedText=' + info.selectionText + '&url=' + url,
+        url: '/src/options/index.html?selectedText=' + info.selectionText + '&url=' + url,
         width: 350,
         height: 300,
         left: 700,
