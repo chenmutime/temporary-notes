0490fe18d0ea3cec61b07a858719968f8828450f
chenmutime <chenmutime@outlook.com>
2023-08-21 23:49:05 +0800

    124123213

--- src/pages/sidepanel/sidepanel.vue
+++ src/pages/sidepanel/sidepanel.vue
@@ -31,7 +31,7 @@ function cleatData(url: string, timestamp: number) {
     contentContainer.contentList.forEach(snippetList => {
         snippetList.forEach(item => {
             if (item.url === url && item.timestamp === timestamp) {
-                contentContainer.contentList.splice(contentContainer.contentList.indexOf(item), 1);
+                snippetList.splice(snippetList.indexOf(item), 1);
             }
         });
     });
@@ -40,12 +40,7 @@ function cleatData(url: string, timestamp: number) {
 function exportData() {
 
 }
-function randomColor() {
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 255);
-    return `rgb(${r},${g},${b})`;
-}
+
 </script>
 
 <template>
@@ -53,19 +48,33 @@ function randomColor() {
         <button @click="fetchData()">刷新</button>
         <button @click="clearAllData()">清除</button>
         <button @click="exportData()">导出</button>
-        <view v-for="(snippetList, index) in contentContainer.contentList" :key="index" style="display: block;margin: 5px 5px;">
+        <view v-for="(snippetList, index) in contentContainer.contentList" :key="index"
+            style="display: block;margin: 5px 5px;">
             <view v-if="snippetList.length > 0">
                 <h3>{{ snippetList[0].url }}</h3>
                 <!-- 根据index获取随机颜色 -->
-                <view :style="{ backgroundColor: randomColor() }">
-                    <view v-for="(snippet, sIndex) in snippetList" :key="sIndex">
-                        <!-- 清除图标，只清除html节点，并不清除实际节点 -->
-                        <button @click="cleatData(snippet.url, snippet.timestamp)">clear</button>
-                        <view style="display: block;margin: 5px 5px;">{{ snippet.selected_text }}</view>
-                        <view style="display: block;margin: 5px 5px;">{{ snippet.input_text }}</view>
-                    </view>
+                <view v-for="(snippet, sIndex) in snippetList" :key="sIndex">
+                    <!-- 清除图标，只清除html节点，并不清除实际节点 -->
+                    <view class="selectedText">{{ snippet.selected_text }}</view>
+                    <view class="inputText">{{ snippet.input_text }}</view>
+                    <button @click="cleatData(snippet.url, snippet.timestamp)">clear</button>
                 </view>
             </view>
         </view>
     </main>
 </template>
+
+<style scoped>  .selectedText {
+      font-size: smaller;
+      color: rgb(200, 200, 200);
+      display: block;
+      margin: 5px 5px;
+  }
+
+  .inputText {
+      font-size: small;
+      color: black;
+      display: block;
+      margin: 5px 5px;
+  }
+</style>
