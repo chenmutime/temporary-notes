4281b684752436842563d2437bae7adf46773279
chenmutime <chenmutime@outlook.com>
2023-08-24 23:23:00 +0800

    12323

--- src/sidepanel/sidepanel.vue
+++ src/sidepanel/sidepanel.vue
@@ -86,6 +86,7 @@ function formatDataToText(contentList: object[]) {
 }
 
 const bg_color_arr: string[] = ["bg-green-50", "bg-yellow-50", "bg-red-50", "bg-lime-50", "bg-violet-50"];
+const title_bg_color_arr: string[] = ["bg-green-100", "bg-yellow-100", "bg-red-100", "bg-lime-100", "bg-violet-100"];
 
 </script>
 
@@ -102,21 +103,29 @@ const bg_color_arr: string[] = ["bg-green-50", "bg-yellow-50", "bg-red-50", "bg-
         <div class="border-b border-1 border-gray-300"></div>
         <view v-for="(snippetList, index) in contentContainer.contentList" :key="index">
             <view v-if="snippetList.length > 0">
-                <div class="rounded overflow-hidden shadow-xl m-4 border-gray-500 border-solid" :class="bg_color_arr[index % 5]">
+                <div class="rounded overflow-hidden shadow-xl m-4 border-gray-500 border-solid"
+                    :class="bg_color_arr[index % 5]">
                     <!-- 标题，自动换行 -->
-                    <div class="font-bold text-sm p-2 text-left break-all" :href="snippetList[0].url">{{ snippetList[0].url }}</div>
-                    <!-- 分割线 -->
-                    <div class="border-b border-gray-300"></div>
+                    <div class="font-bold text-sm p-2 text-left break-all" :class="title_bg_color_arr[index % 5]"
+                        :href="snippetList[0].url">{{ snippetList[0].url }}</div>
+
                     <!-- 根据index获取随机颜色 -->
                     <view v-for="(snippet, sIndex) in snippetList" :key="sIndex">
                         <!-- 清除图标，只清除html节点，并不清除实际节点 -->
                         <div class="p-2 flex">
-                            <img src="../assets/delete.svg" class="h-5 w-5 mt-1 " @click="cleatData(snippet.url, snippet.timestamp)">
                             <div class="mx-1">
-                                <p class="text-gray-400 text-xm text-left break-all">{{ clipSelectedText(snippet.selected_text) }}</p>
+                                <p class="text-gray-400 text-xm text-left break-all">“{{
+                                    clipSelectedText(snippet.selected_text) }}”</p>
                                 <p class="text-gray-700 text-base text-left break-all">{{ snippet.input_text }}</p>
                             </div>
+                            <img src="../assets/delete.svg" class="h-5 w-5 mt-1 "
+                                @click="cleatData(snippet.url, snippet.timestamp)">
                         </div>
+                        <br>
+                        <!-- 分割线 -->
+                        <view v-if="(sIndex+1) < snippetList.length">
+                            <div class="border-b border-gray-300"></div>
+                        </view>
                     </view>
                 </div>
             </view>
